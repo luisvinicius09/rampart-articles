cb52f3baccff740dcf9001c0c0554b32abf59de1
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-15 18:53:03 -0300

    Add name field to Users & add registration controller to devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,7 +13,7 @@ class ArticlesController < ApplicationController
 
   # GET /articles/new
   def new
-    @article = Article.new
+    @article = current_user.articles.build
   end
 
   # GET /articles/1/edit
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
 
     respond_to do |format|
       if @article.save
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
-  has_many :votes
-  
+  has_many :votes, foreign_key: :article_id
+  belongs_to :category
+
 end
