deafe304950d46c9dc2e25922ea54fd8f190e5cd
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-17 13:59:39 -0300

    Create interection with backend

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,2 +1,25 @@
 class VoteController < ApplicationController
+  def create
+    @vote = Vote.new(user_id: current_user.id, article_id: params[:article_id])
+
+    if @vote.save
+      flash[:notice] = 'You upvoted this article!'
+      redirect_to article_path(params[:article_id])
+    else
+      flash[:alert] = "You can't vote in this article!"
+      redirect_to article_path(params[:article_id])
+    end
+  end
+
+  def destroy
+    vote = Vote.find_by(user_id: current_user.id, article_id: params[:article_id])
+    if vote
+      vote.destroy
+      flash[:notice] = 'You downvoted this article!'
+      redirect_to article_path(params[:article_id])
+    else
+      flash[:alert] = "Something went wrong with your vote!"
+      redirect_to article_path(params[:article_id])
+    end
+  end
 end
--- app/helpers/vote_helper.rb
+++ app/helpers/vote_helper.rb
@@ -1,2 +1,10 @@
 module VoteHelper
+  def upvote_or_downvote_btn(article)
+    vote = Vote.find_by(article: article, user_id: current_user.id)
+    if vote
+      link_to('Downvote', article_vote_path(id: vote.id, article_id: article.id), method: :delete)
+    else
+      link_to('Upvote', article_vote_index_path(article_id: article.id), method: :post)
+    end
+  end
 end
