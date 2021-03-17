b5e5503d96a7418977873979f96678e830563e36
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-17 18:27:28 -0300

    Update article styling

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,7 +1,7 @@
 module ArticlesHelper
   def edit_btn(user)
     if user.id == @article.user_id
-      link_to 'Edit', edit_article_path(@article)
+      link_to 'Edit', edit_article_path(@article), class: 'vote-btn'
     end
   end
 end
--- app/helpers/vote_helper.rb
+++ app/helpers/vote_helper.rb
@@ -2,9 +2,9 @@ module VoteHelper
   def upvote_or_downvote_btn(article)
     vote = Vote.find_by(article: article, user_id: current_user.id)
     if vote
-      link_to('Downvote', article_vote_path(id: vote.id, article_id: article.id), method: :delete)
+      link_to('Downvote', article_vote_path(id: vote.id, article_id: article.id), method: :delete, class: 'vote-btn')
     else
-      link_to('Upvote', article_vote_index_path(article_id: article.id), method: :post)
+      link_to('Upvote', article_vote_index_path(article_id: article.id), method: :post, class: 'vote-btn')
     end
   end
 end
