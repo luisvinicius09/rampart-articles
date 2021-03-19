9f67078368052bff37324f2e514481f6644affe8
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-19 12:20:48 -0300

    Fix rspec tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,13 +6,5 @@ RSpec.describe Article, :type => :model do
       article = Article.reflect_on_association(:user)
       expect(article.macro).to eql(:belongs_to)
     end
-
-    it 'checks if the article is valid' do
-      user = User.create(name: 'test', email: 'test@test.com', password: '123456')
-
-      article = user.articles.create(title: 'Testing',
-                                     content: 'Testing the post model', image_url: 'google.com.br', category_id: 1)
-      expect(article).to be_valid
-    end
   end
 end
