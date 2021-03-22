e84415c7c8e9555a0f012703f99c57d0f59eca8f
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-22 14:19:19 -0300

    Remove logic from views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def buttons(sign_in)
+    if sign_in
+      render :partial => 'logged'
+    else
+      render :partial => 'user'
+    end
+  end
 end
--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,7 @@ module ArticlesHelper
   def edit_btn(user)
     link_to 'Edit', edit_article_path(@article), class: 'vote-btn' if user.id == @article.user_id
   end
+
+  def form_error(error)
+  end
 end
