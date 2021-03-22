8b72f72e57e7a62416d6a4c3d31609b6b55bf9be
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-22 13:58:55 -0300

    Add validations to article & user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,11 @@
 class Article < ApplicationRecord
+  validates :title, :presence => true
+  validates :content, :presence => true
   validates :image_url, :presence => true
 
   belongs_to :user
   has_many :votes, foreign_key: :article_id
-  belongs_to :category
+  has_many :category
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, :presence => true
+  validates :name, :presence => true
+  
   has_many :articles, foreign_key: :user_id
   has_many :votes, foreign_key: :user_id
 end
