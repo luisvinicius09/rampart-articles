065bad5731ea23bc4137cce2967eac719bdc8f74
Luis Vinicius <luisvinicius0906@gmail.com>
2021-03-22 14:22:10 -0300

    Fix rubocop errors

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,14 +10,16 @@ RSpec.describe Article, :type => :model do
     it 'checks if the article is valid' do
       user = User.create(name: 'Test', email: 'test@test.com', password: 123456)
       category = Category.create(name: 'Business', priority: 2)
-      article = Article.create(title: 'Test', content: 'Testing', user_id: user.id, image_url: 'test.com', category_id: category.id)
+      article = Article.create(title: 'Test', content: 'Testing',
+                               user_id: user.id, image_url: 'test.com', category_id: category.id)
       expect(article).to be_valid
     end
 
     it 'checks if the article is not valid' do
       user = User.create(name: 'Test', email: 'test@test.com', password: 123456)
       category = Category.create(name: 'Business', priority: 2)
-      article = Article.create(title: nil, content: nil, user_id: user.id, image_url: nil, category_id: category.id)
+      article = Article.create(title: nil, content: nil,
+                               user_id: user.id, image_url: nil, category_id: category.id)
       expect(article).to_not be_valid
     end
   end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -10,15 +10,13 @@ RSpec.describe Vote, :type => :model do
     it 'checks if the vote is valid' do
       user = User.create(name: 'Test', email: 'test@test.com', password: 123456)
       category = Category.create(name: 'Business', priority: 2)
-      article = Article.create(title: 'Test', content: 'Testing', user_id: user.id, image_url: 'test.com', category_id: category.id)
+      article = Article.create(title: 'Test', content: 'Testing',
+                               user_id: user.id, image_url: 'test.com', category_id: category.id)
       vote = Vote.create(user_id: user.id, article_id: article.id)
       expect(vote).to be_valid
     end
 
     it 'checks if the vote is not valid' do
-      user = User.create(name: 'Test', email: 'test@test.com', password: 123456)
-      category = Category.create(name: 'Business', priority: 2)
-      article = Article.create(title: 'Test', content: 'Testing', user_id: user.id, image_url: 'test.com', category_id: category.id)
       vote = Vote.create(user_id: nil, article_id: nil)
       expect(vote).to_not be_valid
     end
